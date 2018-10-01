fd2e87f23e1a71923fd70484beaa6ca547075918
Kohei Matsumoto <matsumoto@irvs.ait.kyushu-u.ac.jp>
2018-10-01 17:13:21 +0900

    Fix output format

--- eps_generator.py
+++ eps_generator.py
@@ -1,12 +1,17 @@
 import sys
 from kivy.app import App
 from kivy.core.window import Window
-from PIL import Image as pi
-from pgmagick import Image
+from PIL import Image as Image_PIL
+from pgmagick import Image as Image_Pg_
 import os
 
 
 class EpsGenerator(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.img_format = "EPS"
+
     def build(self):
         if os.name != "posix" and os.name != "nt":
             print("This Operating System is not Supported")
@@ -18,26 +23,31 @@ class EpsGenerator(App):
     def _on_file_drop(self, window, file_path):
         file_name = ""
         export_path = ""
-        img = pi.open(file_path)
-        img_pg = Image(file_path)
+        img_pil = Image_PIL.open(file_path)
+        img_pg = Image_Pg_(file_path)
 
         if os.name == "posix":
             export_path = "/"
-            file_name = str(img.filename).split("/")[-1].split(".")[0]
-            export_path_parts = str(img.filename).split("/")[0:-1]
+            file_name = str(img_pil.filename).split("/")[-1].split(".")[0]
+            export_path_parts = str(img_pil.filename).split("/")[0:-1]
             del export_path_parts[0]
             for _, path_parts in enumerate(export_path_parts):
                 export_path = export_path + path_parts + "/"
         elif os.name == "nt":
-            file_name = str(img.filename).split("¥")[-1].split(".")[0]
-            export_path_parts = str(img.filename).split("¥")[0:-1]
+            file_name = str(img_pil.filename).split("¥")[-1].split(".")[0]
+            export_path_parts = str(img_pil.filename).split("¥")[0:-1]
             del export_path_parts[0]
             for _, path_parts in enumerate(export_path_parts):
                 export_path = export_path + path_parts + "¥"
         # img = img.resize((int(img.height/2), int(img.width/2)), Image.LANCZOS)
         # img.thumbnail(size, Image.LANCZOS)
         # print(img.size)
-        img_pg.write(export_path + file_name + ".eps3")
+        if self.img_format == "EPS":
+            img_pg.write("EPS:" + export_path + file_name + ".eps")
+        elif self.img_format == "EPS2":
+            img_pg.write("EPS2:" + export_path + file_name + ".eps")
+        elif self.img_format == "EPS3":
+            img_pg.write("EPS3:" + export_path + file_name + ".eps")
         # img.save(export_path + file_name + ".eps2", "EPS")
         return
 
